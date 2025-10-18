11279529d884d9b8bbe93a9812cd9b171bbb0060
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2025-10-18 10:28:46 +0500

    Circumference Calculator created

---
+++ script.js
@@ -0,0 +1,75 @@
+// Taking Input from User :
+
+// 1. Simple Way :
+
+// const userName = prompt("Enter Your Name : ");
+// console.log(userName);
+
+// 2. Professional Way :
+
+// let userName;
+// document.getElementById("myButton").onclick = function () {
+//   userName = document.getElementById("inputField").value;
+//   document.getElementById("h3").textContent = `Hello, ${userName}! `;
+//   console.log(userName);
+// };
+
+// ----------------------------------------------------
+
+// Type Casting and Constant Variables :
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(typeof x, "-->", x);
+// console.log(typeof y, "-->", y);
+// console.log(typeof z, "-->", z);
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(typeof x, "-->", x);
+// console.log(typeof y, "-->", y);
+// console.log(typeof z, "-->", z);
+
+// let x = 0;
+// let y = 0;
+// let z = 0;
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(typeof x , "-->" , x);
+// console.log(typeof y , "-->" , y);
+// console.log(typeof z , "-->" , z);
+
+// -------------------------------------------------
+
+// Constant (const) Variables :
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// document.getElementById("myButton").onclick = function () {
+//   radius = document.getElementById("inputField").value;
+//   radius = Number(radius);
+//   circumference = (2 * PI * radius).toFixed(2);                 // 2 decimals tak round ho gii value 
+//   document.getElementById(
+//     "p"
+//   ).textContent = `The circumference of a circle with radius ${radius} cm is ${circumference} cm.`;
+// };
+
+
+
