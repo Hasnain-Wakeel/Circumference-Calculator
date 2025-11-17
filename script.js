7b35644e5faca7d6ba68493a8c8fbde0728b8204
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2025-11-17 10:28:44 +0500

    Modified Calculator UX

--- script.js
+++ script.js
@@ -44,30 +44,41 @@ const PI = 3.14159;
 let radius;
 let circumference;
 
-document.getElementById("myButton").onclick = function () {
-  const input = document.getElementById("inputField");
-  const output = document.getElementById("p");
+// Targeting Elements by their IDs:
+const myButton = document.getElementById("myButton");
+const input = document.getElementById("inputField");
+const output = document.getElementById("p");
 
-  // Check if Input is Empty or not a Valid Number
+myButton.onclick = function () {
+
+  // Checks, if Input is Empty or not a Valid Number :
+
   if (input.value.trim() === "" || isNaN(input.value) || input.value < 0 ) {
     output.textContent = "Please enter a valid radius!";
-    output.style.color = "#d32f2f";         // red for error
-    input.style.borderColor = "#d32f2f";    // red border for invalid input
-    return;
+    output.style.color = "#ff0000ff";         // red text for invalid input
+    input.style.borderColor = "#ff0000ff";    // red border for invalid input
+
+    resetFunction(input);        // clears input and focuses the input field again
+
+    return;     // exits the function if input is invalid
   }
+
+  // Calculating Circumference :
 
   radius = Number(input.value);
   circumference = (2 * PI * radius).toFixed(2);
 
-  // Show result
+  // Show result :
+
   output.textContent = `The circumference of a circle with radius ${radius} cm is ${circumference} cm.`;
   output.style.color = "#1e40af";             // blue for result
   input.style.borderColor = "#3b82f6";        // restore normal border color
 
   resetFunction(input);
+
 };
 
 function resetFunction(input) {
-  input.value = "";     // clear input
+  input.value = "";     // clears input
   input.focus();        // auto-focus again
 }
